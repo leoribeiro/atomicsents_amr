ae4969e1debf44beb43de0ed94dd010ce9af6b2b
dannlos <mail@danilowalenta.com>
2022-11-24 21:50:48 +0100

    Run pipeline on third and forth dataset

---
+++ create_json_from_source.py
@@ -0,0 +1,37 @@
+import json
+
+
+def open_file(filename):
+    with open(filename) as f:
+        lines = f.readlines()
+    return lines
+
+
+def create_json(summary, name_of_sxus, sxus, name_of_output):
+    outputDict = []
+    for i, l in enumerate(summary):
+        outputDict.append(
+            {
+                'instance_id': name_of_output[:-10] + "-" + str(i),
+                'summary': l.replace('\n', ''),
+                name_of_sxus: sxus[i].replace('\n', '').split("\t"),
+            }
+        )
+
+    jsonString = json.dumps(outputDict)
+    jsonFile = open(name_of_output, "w")
+    jsonFile.write(jsonString)
+    jsonFile.close()
+
+
+summary = open_file('data/PyrXSum/references.txt')
+stus = open_file('data/PyrXSum/STUs.txt')
+scus = open_file('data/PyrXSum/SCUs.txt')
+create_json(summary, 'stus', stus, 'pyrxsum-stus.json')
+create_json(summary, 'scus', scus, 'pyrxsum-scus.json')
+
+summary = open_file('data/REALSumm/references.txt')
+stus = open_file('data/REALSumm/STUs.txt')
+scus = open_file('data/REALSumm/SCUs.txt')
+create_json(summary, 'stus', stus, 'realsumm-stus.json')
+create_json(summary, 'scus', scus, 'realsumm-scus.json')
