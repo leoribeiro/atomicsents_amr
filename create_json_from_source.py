690a5f0dc064925736385f09daa859736d708e7d
dannlos <mail@danilowalenta.com>
2022-11-26 19:19:25 +0100

    create json from text files for PyrXSum and REALSumm dataset

--- create_json_from_source.py
+++ create_json_from_source.py
@@ -7,14 +7,14 @@ def open_file(filename):
     return lines
 
 
-def create_json(summary, name_of_sxus, sxus, name_of_output):
+def create_json(summary, name_of_sxus, sxus, name_of_output, name_of_instance):
     outputDict = []
     for i, l in enumerate(summary):
         outputDict.append(
             {
-                'instance_id': name_of_output[:-10] + "-" + str(i),
-                'summary': l.replace('\n', ''),
-                name_of_sxus: sxus[i].replace('\n', '').split("\t"),
+                'instance_id': name_of_instance + "-" + str(i),
+                'summary': l.replace('\n', ''),#.replace('\"', '').replace('\u2019', '`').replace('\u00a3', 'Pound'),
+                name_of_sxus: sxus[i].replace('\n', '').split("\t"),#.replace('\"', '').replace('\u2019', '`').replace('\u00a3', 'Pound').split("\t"),
             }
         )
 
@@ -27,12 +27,12 @@ def create_json(summary, name_of_sxus, sxus, name_of_output):
 summary = open_file('data/PyrXSum(Source)/references.txt')
 stus = open_file('data/PyrXSum(Source)/STUs.txt')
 scus = open_file('data/PyrXSum(Source)/SCUs.txt')
-create_json(summary, 'stus', stus, 'data/pyrxsum/pyrxsum-stus.json')
-create_json(summary, 'scus', scus, 'data/pyrxsum/pyrxsum-scus.json')
+create_json(summary, 'stus', stus, 'data/pyrxsum/pyrxsum-stus.json', 'pyrxsum')
+create_json(summary, 'scus', scus, 'data/pyrxsum/pyrxsum-scus.json', 'pyrxsum')
 
 # Create Json out of source REALSumm data
 summary = open_file('data/REALSumm(Source)/references.txt')
 stus = open_file('data/REALSumm(Source)/STUs.txt')
 scus = open_file('data/REALSumm(Source)/SCUs.txt')
-create_json(summary, 'stus', stus, 'data/realsumm/realsumm-stus.json')
-create_json(summary, 'scus', scus, 'data/realsumm/realsumm-scus.json')
+create_json(summary, 'stus', stus, 'data/realsumm/realsumm-stus.json', 'realsumm')
+create_json(summary, 'scus', scus, 'data/realsumm/realsumm-scus.json', 'realsumm')
