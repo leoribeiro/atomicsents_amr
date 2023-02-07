5379e38757b80fe2e699a68e7b5a4b3f03361b21
tristanratz <triratz@aol.com>
2023-02-07 09:56:00 +0100

    Implement generation and add generated samples (SGUs)

---
+++ scripts/gpt/generate_sgus.py
@@ -0,0 +1,30 @@
+import openai
+import json
+import pandas as pd
+
+ft_model = 'curie:ft-personal-2023-02-06-15-05-21' # ft-yWG8yb5cL8E1du9igUeaFk4O
+
+def get_path(ds, fn = None):
+    print('Load data', ds,'...')
+    return "../../eval_interface/src/data/" + ds + "/" + (fn if fn != None else ds + "-scus") + ".json"
+
+def get_save_path(ds, fn = None):
+    print('Save data', ds,'...')
+    return "../../eval_interface/src/data/" + ds + "/" + (fn if fn != None else ds + "-sgus") + ".json"
+
+
+def generate(ds):
+    fn = get_path(ds)
+    with open(fn) as f:
+        dataset = []
+        d = json.load(f)
+        for idx, s in enumerate(d):
+            print(idx, "/", len(d), "samples")
+            sample = { "summary": s["summary"], "instance_id": s["instance_id"] }
+            prompt = s["summary"].replace('<t> ','').replace(' </t>','').replace(" . ", ". ")
+            res = openai.Completion.create(model=ft_model, prompt=prompt + ' ->', max_tokens=1000, stop=" END\n")
+            sample['sgus'] = res['choices'][0]['text'].replace(" END\n", "").lstrip().replace(" . ", ". ").split(" # ")
+            dataset.append(sample)
+        json.dump(dataset, open(get_save_path(ds), "w"))
+
+generate("pyrxsum")
--- scripts/gpt/generate_training_gpt.py
+++ scripts/gpt/generate_training_gpt.py
@@ -9,17 +9,22 @@ def load_json(fn):
         d = json.load(f)
         for s in d:
             sample = {}
-            sample['prompt'] = s["summary"]
-            response = ""
-            for scu in s["scus"]:
-                response = response + scu + " # "
+            sample['prompt'] = s["summary"] + " ->"
+            response = " "
+            for id, scu in enumerate(s["scus"]):
+                response = response + scu
+                if id < len(s["scus"])-1:
+                    response = response + " # "
+                else:
+                    response = response + " END\n"
+
             sample["completion"] = response
             data.append(sample)
             
 
 def get_path(ds, fn = None):
     print('Load data', ds,'...')
-    return "eval_interface/src/data/" + ds + "/" + (fn if fn != None else ds + "-scus") + ".json"
+    return "../../eval_interface/src/data/" + ds + "/" + (fn if fn != None else ds + "-scus") + ".json"
 
 
 load_json(get_path("tac08", "tac2008-scus-sp"))
@@ -29,7 +34,7 @@ print("Shuffle data")
 random.shuffle(data)
 
 print("Write file...")
-with open("data/gpt_training.jsonl", "w") as fp:
+with open("../../data/gpt_training.jsonl", "w") as fp:
     writer = jsonlines.Writer(fp) 
     writer.write_all(data)
     writer.close()
